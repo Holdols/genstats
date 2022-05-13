a2b4d71dfd9da33403cfc2a92e28fd44cf1bbe3d
louisemoelgaard <louisemolgaard99@gmail.com>
2022-05-13 19:03:36 +0200

    Emils corrections

--- R/LTFH.R
+++ R/LTFH.R
@@ -24,10 +24,9 @@ calc_distribution = function(sigma){
 #' @param phenos A binary vector containing the phenotype for each family member
 #' of the form \cr c(p_subject, p_parent1, p_parent2, p_sibling1, ... ,p_siblingN) \cr
 #' where p_familymember is a binary value (1 or 2) \cr
-#' @param N Amount of rows in the matrix that the estimations are saved in
 #' @param K The prevalance of trait
 #' @param s_val The starting value of estimates
-#' @param min_run The minimum amount of runs to do after burn in
+#' @param start_run Number of iterations before convergence is expected
 #' @param all_est If TRUE return the value for each iteration after burn in
 #' @return A vector containing LTFH estimate of liabilities of the form \cr
 #' c(genetic_liability_subject, liability_subject, liability_parent1, \cr
@@ -35,26 +34,22 @@ calc_distribution = function(sigma){
 #' @examples
 #' LTFH(get_cov(0.5, n_sib = 1), c(1, 1, 0, 0))
 #' @export
-LTFH <- function(covmat, phenos, N=10000, K = 0.05, s_val = 0, min_run=400, all_est=FALSE){
 
-
+gibbs_sampl <- function(covmat, phenos, K = 0.05, s_val = 0, start_run=500, all_est=FALSE){
   k = nrow(covmat)
-
   const_list = calc_distribution(covmat)
 
-  liabil = matrix(s_val, nrow = N, ncol = k) # i formen c(l_g, l, l_p1, l_p2)
-
+  # i formen c(l_g, l, l_p1, l_p2)
   current_liabil = rep(s_val, k)
-
-  start_run = 500
-  s = 0
-  p = 0
+  liabil_list = list(current_liabil)
+  liabil = matrix(s_val, nrow=1, ncol=k)
+  total_runs = 1
 
   while(TRUE) {
-    p = p +1
-    for (i in 2:start_run) {
-      s = s + 1
+    if (total_runs>2 && all(sapply(1:k, function(i) sd(liabil[,i])) / sqrt(total_runs) < 0.01)) break
+    for (i in 1:start_run) {
       for (j in 1:k) {
+
         # Udregner parametre
         sigma = const_list[[j]]$sigma
         mu_mult_ = const_list[[j]]$mu_mult
@@ -65,36 +60,23 @@ LTFH <- function(covmat, phenos, N=10000, K = 0.05, s_val = 0, min_run=400, all_
           current_liabil[j] = rnorm(1, mu, sqrt(sigma))
         }
         else {
-          crit <- qnorm(1-K)
-          crit_bound = pnorm(crit, mu, sqrt(sigma))
+          if (phenos[j-1] == 'numeric') {phen = phenos[j-1]} else {phen = phenos[j-1][1]}
+          crit_bound = pnorm(qnorm(1-K), mu, sqrt(sigma))
+          interval = phen * c(crit_bound, 1) + (1-phen)*c(0, crit_bound)
 
-          interval_2 = c(0, crit_bound)
-          interval_1 = c(crit_bound, 1)
-
-          phen = phenos[j-1]
-          interval = phen * interval_1 + (1-phen)*interval_2
           U = runif(1,interval[1], interval[2])
           current_liabil[j] = qnorm(U, mu, sqrt(sigma))
         }
-
-
-        if(s > start_run){
-          liabil[i+start_run*(p-2),] = current_liabil
-        }
       }
+      liabil_list[[length(liabil_list)+1]] = current_liabil
+      total_runs = total_runs + 1
     }
-
-    if(s>(start_run+min_run)){
-      end_val = s-start_run
-      if(sd(liabil[1:end_val,1])/sqrt(length(liabil[1:end_val,1])) < 0.1){
-        if (all_est){
-          return(liabil[1:end_val,])
-        }
-        return(colMeans(liabil[1:end_val,]))
-
-      }
-    }
-
+    liabil = liabil_list %>% do.call('rbind',.)
   }
+  if (all_est){
+    return(liabil[-(1:(start_run+1)),])
+  }
+  return(colMeans(liabil[-(1:(start_run+1)),]))
 }
+
 
