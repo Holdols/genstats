dc48e8c0a641d8ff0e5abb18abf23b9efef8c518
louisemoelgaard <louisemolgaard99@gmail.com>
2022-05-15 16:40:33 +0200

    Small fix

--- R/assign_LTFH.R
+++ R/assign_LTFH.R
@@ -7,17 +7,18 @@
 #' @param n_sib Amount of siblings
 #' @param h2 The heritability
 #' @return Tibble with estimated liabilities
-estimate_conf = function(unique_comb, K=0.5, h2=0.5, n_sib=0){
+#' @importFrom magrittr "%>%"
+estimate_conf = function(unique_comb, n_sib=0, K=0.5, h2=0.5){
   k = ncol(unique_comb)
   covmat = get_cov(h2, n_sib)
-
-
   estimates = matrix(NA, nrow = nrow(unique_comb), ncol=4+n_sib)
+
+
   for (i in 1:nrow(unique_comb)){
-    estimates[i,] = LTFH(covmat, unique_comb[i,], K=K)
+    estimates[i,] = gibbs_sampl(covmat, unlist(unique_comb[i,], use.names = FALSE), K)
   }
 
-  out = unique_comb %>% bind_cols('l_g_est_0'=estimates[,1])
+  out = unique_comb %>% dplyr::bind_cols('l_g_est_0'=estimates[,1])
 
   return(out)
 }
@@ -31,18 +32,22 @@ estimate_conf = function(unique_comb, K=0.5, h2=0.5, n_sib=0){
 #' @param n_sib Amount of siblings
 #' @param h2 The heritability
 #' @return Estimated liabilities
+#' @importFrom magrittr "%>%"
 #' @export
 LTFH = function(data, n_sib=0, K=0.05, h2=0.5){
 
-  unique_comb = data %>% select(., contains('pheno')) %>%
-    relocate(order = get_names(c("phen"), n_sib)) %>%
+
+  unique_comb = data %>% dplyr::select(., dplyr::contains('pheno')) %>%
+    dplyr::relocate(get_names(c("pheno"), n_sib)) %>%
     unique(.)
 
 
-  estimated_liabil = estimate_conf(unique_comb, K, n_sib, h2)
-  full_data = left_join(data, estimated_liabil,
-                        by=get_names(c("phen"),n_sib))
-  return(as_tibble(full_data))
+  estimated_liabil = estimate_conf(unique_comb, n_sib, K, h2)
+
+
+  full_data = dplyr::left_join(data, estimated_liabil,
+                        by=get_names(c("pheno"),n_sib))
+  return(full_data)
 }
 
 
