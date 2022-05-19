9a596660ffd54a00cd3de639998195528617c2f6
OleSahlholdt <o.sahlholdt@gmail.com>
2022-05-19 11:38:03 +0200

    reverted

--- R/estimate_liabilities.R
+++ R/estimate_liabilities.R
@@ -9,7 +9,7 @@
 estimate_liabilities = function(pheno, K=0.05, n_sib=0, h2=0.5){
   unique_comb = unique(pheno)
   estimated_liabil = estimate_conf(unique_comb, h2, n_sib, K)
-  key = get_names(c("pheno"),n_sib)
+  key = get_names(c("phen"),n_sib)
   full_data = left_join(as_tibble(pheno),as_tibble(estimated_liabil),by=key)
   order = get_names(c("phen", 'est_l_g'), n_sib)
   return(as_tibble(full_data) %>% relocate(order))
