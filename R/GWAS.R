6445c7ddbd740c0d99825e7c15ed4b5d1516f2c7
OleSahlholdt <o.sahlholdt@gmail.com>
2022-05-24 09:25:17 +0200

    Added LogReg

--- R/GWAS.R
+++ R/GWAS.R
@@ -3,8 +3,9 @@
 #' This function is heavily build onto bigstatsr::big_univLinReg. For more documentation check \cr https://privefl.github.io/bigstatsr/reference/big_univLinReg.html
 #' @param G A file backed matrix with SNP data.
 #' @param y A vector containing targets for each position.
-#' @param ncores Amount of cores to use.
 #' @param p The significance level.
+#' @param logreg if TRUE do logistic regression, else Linear regression.
+#' @param ncores Amount of cores to use.
 #' @return A matrix containing: \cr
 #'  - The slopes of each regression \cr
 #'  - The standard errors of each slope \cr
@@ -12,8 +13,13 @@
 #'  - The p-values for each slope \cr
 #'  - And a binary vector saying if the position is causal or not given p
 #' @export
-GWAS <- function(G, y, ncores = 1, p){
-  lm = bigstatsr::big_univLinReg(X = G, y.train = y, ncores = ncores)
+GWAS <- function(G, y, p, logreg = FALSE, ncores = 1){
+  if (logreg == TRUE){
+    lm = bigstatsr::big_univLogReg(X = G, y01.train = y, ncores = ncores)
+  }
+  else{
+    lm = bigstatsr::big_univLinReg(X = G, y.train = y, ncores = ncores)
+  }
   p_vals = predict(lm, log10 = FALSE)
   causal_estimate = ifelse(p_vals <=p, 1, 0)
   return(cbind(lm, p_vals, causal_estimate))
