2685b393f5fe3bfe8105b4b36038fa6d4a27b136
OleSahlholdt <o.sahlholdt@gmail.com>
2022-05-25 13:15:17 +0200

    logreg

--- R/PRS_cross.R
+++ R/PRS_cross.R
@@ -1,4 +1,4 @@
-PRS_cross <- function(data, y01, cross_folds){
+PRS_cross <- function(data, y01, cross_folds, LogReg = FALSE){
   folds = list()
   G = data$genotypes
   indexes <- rows_along(G)
@@ -10,13 +10,19 @@ PRS_cross <- function(data, y01, cross_folds){
   }
 
   out = list()
+  N = nrow(G)
   for (i in 1:cross_folds){
 
-    N = nrow(G)
+
     ind_test <- folds[[i]]
     ind_train <- setdiff(rows_along(G), ind_test)
+    if (LogReg == TRUE){
+      bigstatsr::big_univLogRegg(G, y.train = y01[ind_train], ind.train = ind_train)
+    }
+    else {
+      gwas_train <- bigstatsr::big_univLinReg(G, y.train = y01[ind_train], ind.train = ind_train)
+    }
 
-    gwas_train <- bigstatsr::big_univLinReg(G, y.train = y01[ind_train], ind.train = ind_train)
     pval <- -predict(gwas_train)
     pval_thrs <- seq(0, 4, by = 0.5)
     prs <- bigsnpr::snp_PRS(G, betas.keep = gwas_train$estim,
