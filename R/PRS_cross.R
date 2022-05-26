67805c9653fd97ddb6ba0138f26029d4ed61695a
louisemoelgaard <louisemolgaard99@gmail.com>
2022-05-26 11:28:42 +0200

    Update

--- R/PRS_cross.R
+++ R/PRS_cross.R
@@ -1,5 +1,9 @@
 #'  Uses cross validation to train and estimate PRS for each fold
 #'
+#' The funcion returns list of the PRS estimated of each fold for each threshold. The function will train on the remaining data and test for each fold.
+#' The function uses GWAS to estimate the casual SNP and uses these the find the PRS. The out will be a list containing matrices with a column for each threshold.
+#' The interpretation of this treshold is that a given effect of a SNP will not be included if the -log10 transformation of the p-value is is smaller than the threshold.
+#' Fx. a value of 3 will correspond to the p value being smaller than 0.001.
 #' @param data List generated from gen_sim.
 #' @param y The target vector. Could either be estimated liabilities from LTFH or phenotypes.
 #' @param cross_folds Number of folds in cross validation.
