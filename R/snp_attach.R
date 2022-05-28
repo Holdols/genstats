eb02b6b7b71f6f654aa4f4f1ae8348638748898a
OleSahlholdt <o.sahlholdt@gmail.com>
2022-05-28 12:36:41 +0200

    Update

---
+++ R/snp_attach.R
@@ -0,0 +1,11 @@
+#' Attach rds_file
+#'
+#' For ease of use this function is added
+#' It is the exact same as bigsnpr::snp_attach see ?bigsnpr::snp_attach for further documentation
+#'
+#' @param rds_file An rds file
+#' @return the values from the rds file
+#' @export
+snp_attach <- function(rds_file){
+  return(bigsnpr::snp_attach(rds_file))
+}
